f29090b8041dbdf62902c9881bdc85fcf086d428
wangsong <vmaps@qq.com>
2018-12-07 16:49:04 +0800

    vue element业务流程完成，去掉easyui

--- src/main/resources/static/html/js/include.js
+++ src/main/resources/static/html/js/include.js
@@ -11,18 +11,11 @@ var htmlURL = url+ "/html" ;
 //var htmlURL = "/html";
 
 
-document.write("<script type='text/javascript' src='"+htmlURL+"/js/jquery-easyui-1.5/jquery.min.js'></script>");
-document.write("<script type='text/javascript' src='"+htmlURL+"/js/jquery-easyui-1.5/jquery.cookie.js'></script>");
-document.write("<script type='text/javascript' src='"+htmlURL+"/js/jquery-easyui-1.5/jquery.easyui.min.js'></script>");
-document.write("<script type='text/javascript' src='"+htmlURL+"/js/jquery-easyui-1.5/locale/easyui-lang-zh_CN.js'></script>");
+document.write("<script type='text/javascript' src='"+htmlURL+"/js/jquery.min.js'></script>");
+document.write("<script type='text/javascript' src='"+htmlURL+"/js/jquery.cookie.js'></script>");
 document.write("<script type='text/javascript' src='"+htmlURL+"/js/method.js'></script>");
 
-document.write("<link rel='stylesheet' type='text/css' href='"+htmlURL+"/css/jquery-easyui-1.5/themes/default/easyui.css'>");
-document.write("<link rel='stylesheet' type='text/css' href='"+htmlURL+"/css/jquery-easyui-1.5/themes/icon.css'>");
 
-document.write("<link rel='stylesheet' type='text/css' href='"+htmlURL+"/css/jquery-easyui-1.5/themes/icon.css'>");
-document.write("<link rel='stylesheet' type='text/css' href='"+htmlURL+"/css/jquery-easyui-1.5/themes/icon.css'>");
-document.write("<link rel='stylesheet' type='text/css' href='"+htmlURL+"/css/jquery-easyui-1.5/themes/icon.css'>");
 
 
 document.write('<script src="https://unpkg.com/vue/dist/vue.js"></script>');
